2ac1155ed66947707ca952f7248f5a17ac94bfcd
YuvalEllins <yuvalell@post.bgu.ac.il>
2024-06-11 12:00:27 +0300

    version last

--- dev/supply_presentation/Main.java
+++ dev/supply_presentation/Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        boolean configured = false;
         boolean system_on = true;
         Scanner scanner = new Scanner(System.in);
         int user_input;
@@ -15,13 +16,13 @@ public class Main {
         Calendar calendar = Calendar.getInstance();
         Supply supply = new Supply(calendar.getTime(),calendar);
         System_user system_user = new System_user(supply);
-        configuration.configure(supply);
         while(system_on) {
             System.out.print("Hello, welcome to supply system, please identify by entering number of the option:\n");
             System.out.print("1.Manager\n");
             System.out.print("2.Worker\n");
             System.out.print("3.Storekeeper\n");
-            System.out.print("4.Exit system\n");
+            System.out.print("4.load data of system\n"); //not for ordinary users,required option from assignment
+            System.out.print("5.Exit system\n");
             user_input = scanner.nextInt();
             switch (user_input) {
                 case 1 -> {
@@ -61,6 +62,10 @@ public class Main {
                         System.out.print("Credentials are incorrect, please try again\n");
                 }
                 case 4 ->{
+                    if(!configured)
+                        configuration.configure(supply);
+                }
+                case 5 ->{
                     system_on = false;
                 }
                 default -> {
